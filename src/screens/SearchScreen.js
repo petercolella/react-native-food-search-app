ab206bada4b0bba8e2c9dbd59574163cf2708dc7
petercolella <peter.joseph.colella@gmail.com>
2020-05-22 21:29:15 -0400

    Section 8 93

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
+  const handleChange = newTerm => {
+    setTerm(newTerm);
+  };
+
+  const handleSubmit = () => {
+    console.log(term, 'was submitted');
+  };
+
   return (
     <View>
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={handleChange}
+        onTermSubmit={handleSubmit}
+      />
+      <Text>Search Screen: {term}</Text>
     </View>
   );
 };
