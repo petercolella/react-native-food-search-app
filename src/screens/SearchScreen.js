9727ce54b7ae80bf0d2f9a94fa7ebdbe42a75029
petercolella <peter.joseph.colella@gmail.com>
2020-05-23 22:11:10 -0400

    Section 9 97

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,26 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const handleChange = newTerm => {
     setTerm(newTerm);
   };
 
-  const handleSubmit = () => {
-    console.log(term, 'was submitted');
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'atlanta'
+        }
+      });
+      setErrorMessage(false);
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log(err.message);
+      setErrorMessage(true);
+    }
   };
+
+  useEffect(() => {
+    searchApi('continental');
+  }, []);
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={handleChange}
-        onTermSubmit={handleSubmit}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Screen: {term}</Text>
+      {errorMessage && <Text>Something went wrong!</Text>}
+      <Text>We have found {results.length} results.</Text>
     </View>
   );
 };
