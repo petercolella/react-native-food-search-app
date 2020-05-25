338bba98be85c7db2b1e34271d7b10be78184c12
petercolella <peter.joseph.colella@gmail.com>
2020-05-24 23:38:03 -0400

    Section 10 100

---
+++ src/hooks/useResults.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'atlanta'
+        }
+      });
+      setErrorMessage(false);
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log(err.message);
+      setErrorMessage(true);
+    }
+  };
+
+  useEffect(() => {
+    searchApi('continental');
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,37 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(false);
+  const [searchApi, results, errorMessage] = useResults();
 
   const handleChange = newTerm => {
     setTerm(newTerm);
   };
-
-  const searchApi = async searchTerm => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'atlanta'
-        }
-      });
-      setErrorMessage(false);
-      setResults(response.data.businesses);
-    } catch (err) {
-      console.log(err.message);
-      setErrorMessage(true);
-    }
-  };
-
-  useEffect(() => {
-    searchApi('continental');
-  }, []);
 
   return (
     <View>
